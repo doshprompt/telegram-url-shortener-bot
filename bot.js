967a2bdafd27f176a236ae2494562ee130bc6578
Rahul Doshi <rdoshi@netflix.com>
2016-05-04 15:20:34 -0700

    add ability to expand urls

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const Bot = require('node-telegram-bot-api-upgrades');
 
 const validator = require('validator');
 const TinyURL = require('tinyurl');
+const unshort = require('url-unshort');
 const symbols = require('log-symbols');
 
 const bot = new Bot(process.env.TOKEN, {
@@ -18,6 +19,7 @@ bot.onText(/^\/start$/, msg => {
 
 bot.onText(/^\/shorten (.+)$/, (msg, match) => {
     let url = match[1];
+
     if (validator.isURL(url)) {
         TinyURL.shorten(prefixProtocol(url), r => {
             bot.sendMessage(msg.from.id, r);
@@ -27,6 +29,23 @@ bot.onText(/^\/shorten (.+)$/, (msg, match) => {
         bot.sendMessage(msg.chat.id, 'Sorry, that\'s not a vaid URL :(');
         console.log(symbols.error, url);
     }
+});
+
+bot.onText(/^\/shorten (.+)$/, (msg, match) => {
+    let shortUrl = match[1];
+
+    unshort.expand(shortUrl, (err, url) => {
+        if (err) {
+            console.log(symbols.error, err); 
+        } else {
+            if (url) {
+                bot.sendMessage(msg.from.id, url);
+                console.log(symbols.success, shortUrl, '-->', url);
+            } else {
+                console.log(symbols.error, shortUrl);
+            }
+        }
+    });
 });
 
 console.log(symbols.info, 'bot server started...');
